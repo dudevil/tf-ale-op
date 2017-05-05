a809d3b43ac92b2ab47dd0f8a18fbd8e8096e232
dudevil <ovcharenko.ser@gmail.com>
2017-05-05 18:46:36 +0300

    Clean up op code

--- src/ale.cc
+++ src/ale.cc
@@ -1,13 +1,11 @@
 #include <ale_interface.hpp>
-
 #include "tensorflow/core/framework/op.h"
-#include "tensorflow/core/framework/op_kernel.h"
-#include "tensorflow/core/framework/register_types.h"
-#include "tensorflow/core/framework/shape_inference.h"
-#include "tensorflow/core/framework/tensor_shape.h"
-#include "tensorflow/core/platform/default/logging.h"
 #include "tensorflow/core/platform/env.h"
+#include "tensorflow/core/framework/op_kernel.h"
+#include "tensorflow/core/framework/tensor_shape.h"
+#include "tensorflow/core/framework/register_types.h"
 #include "tensorflow/core/lib/random/simple_philox.h"
+#include "tensorflow/core/framework/shape_inference.h"
 #include "tensorflow/core/util/guarded_philox_random.h"
 
 using namespace tensorflow;
@@ -26,7 +24,7 @@ REGISTER_OP("Ale")
     .SetShapeFn([](::tensorflow::shape_inference::InferenceContext* c) {
       c->set_output(0, c->Scalar());
       c->set_output(1, c->Scalar());
-      //      c->set_output(2, c->MakeShape({210, 160, 3}));
+      // no shape inference for screen, because we don't know screen dimensions yet
       return Status::OK();
     });
 
@@ -64,24 +62,25 @@ class AleOp : public OpKernel {
     OP_REQUIRES(context, legalActions_.find(action) != legalActions_.end(),
                 errors::InvalidArgument("Action is out of legal actions range."));
 
-    auto w = ale_.getScreen().width();
-    auto h = ale_.getScreen().height();
+    const int w = ale_.getScreen().width();
+    const int h = ale_.getScreen().height();
     std::vector<unsigned char> screen_buff;
-    screen_buff.reserve(w * h * 3);
 
     auto local_gen = generator_.ReserveSamples32(1);
     random::SimplePhilox random(&local_gen);
-    auto to_repeat = frameskip_min_ + random.Uniform(frameskip_max_ - frameskip_min_);
+    int to_repeat = frameskip_min_ + random.Uniform(frameskip_max_ - frameskip_min_);
 
     float r = 0.0;
     for(;to_repeat > 0; --to_repeat){
       r += ale_.act(action);
     }
 
-    auto done = ale_.game_over();
+    bool done = ale_.game_over();
     ale_.getScreenRGB(screen_buff);
 
-    if(done) ale_.reset_game();
+    if(done) {
+      ale_.reset_game();
+    }
     
     Tensor* reward_tensor = NULL;
     Tensor* done_tensor = NULL;
@@ -98,7 +97,6 @@ class AleOp : public OpKernel {
     auto output_d = done_tensor->scalar<float>();
     auto output_s = screen_tensor->flat<unsigned char>();
 
-    // LOG(INFO) << "screen size: " << screen_buff.size();
     output_r(0) = r;
     output_d(0) = done;
     std::copy_n(screen_buff.begin(), h * w * 3,
